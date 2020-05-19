24e2bdfba49a9c83daf05d80ed369d7ab9e9aaec
priyarazorbee <priya@razorbee.com>
2020-05-19 11:19:58 +0530

    new

--- tv/src/App.js
+++ tv/src/App.js
@@ -1,42 +1,56 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import './styles/foundation.min.css';
-import './styles/custom.css';
-import Routes from './routes';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import ReactDOM from 'react-dom';
 
-import MobileHeader from './components/MobileHeader/MobileHeader';
-
-
-class App extends Component {
-
-  constructor(){
-    super();
-    this.state={
-      appName: "Razorbee",
-      home: false
+class App extends React.Component {
+  constructor(props){
+    super(props) 
+    this.state = {
+      assets: [],
+      index: 0, 
+      timeout:1000
     }
   }
+   fetchAssetObj=()=> {
+    let {assets, index} = this.state;
+    const context = this;
+    const currentObj = this.state.assets.data.assets ? this.state.assets.data.assets[index]:null;
+       
+    index = index+1;
+    if (!assets.data.assets || index == assets.data.assets.length){
+      index =0 ;
+    }
+    this.setState({index:index});
+    setTimeout(context.fetchAssetObj, 5000);
+  }
+   componentDidMount() {
+    const context = this;
+    fetch('./play.json')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ assets: data, index:0});
+      setTimeout(context.fetchAssetObj, 5000);
+              console.log(this.state); 
+    })
+    .catch(console.log)
+  }
 
-  render() {
-    return (
-      <div className="off-canvas-wrapper">
-      <div className="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-
-     
-
-        <div className="off-canvas-content" data-off-canvas-content>
-          <MobileHeader name={this.state.appName}/>
-          <Header name={this.state.appName}/>
-          <Routes name={this.state.appName}/>
-          <hr/>
-         <Footer/>
-        </div>
-      </div>
-    </div>
+  render(){  
+       
+     const {assets, index} = this.state;
+    
+ 
+       return (
+      
+        Object.keys(this.state.object).map((key, i) => (
+          <p key={i}>
+            <span>Key Name: {key}</span>
+            <span>Value: {assets.object[key]}</span>
+          </p>
+        )
+      )
     );
   }
 }
-
+           
 export default App;
